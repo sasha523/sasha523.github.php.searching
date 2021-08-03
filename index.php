cf1dac15577cb7c6c6e39b9396c7d9eb656934b8
Alexander <75177317+sasha523@users.noreply.github.com>
2021-08-03 18:43:53 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
 <form action="search.php" method="post">
 <input id="mySearch" type="text" name="search" value="Search.." onFocus="if(this.value=='Search..') this.value='';" onblur="if(this.value=='') this.value='Search..'">
-<input class="btn" type="submit" name="submit">
+<input class="btn" type="submit" name="submit" value="Найти">
 	
 </form>
 
